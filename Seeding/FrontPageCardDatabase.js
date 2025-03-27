ae237b521f42cae265c596e30ad5f3a2c15304a4
Prasanth <prasanth76200@gmail.com>
2025-03-27 12:04:23 +0530

    Prasanth Changes

---
+++ Seeding/FrontPageCardDatabase.js
@@ -0,0 +1,62 @@
+export const Data=[
+    {
+        
+        id:1,
+        image:{},
+        restName:"banana leaf",
+        restStarRating:3,
+        restTime:"11pm",
+        restType:"chinese",
+        restLocalAddress:"saram",
+        restCity:"pondicherry",
+        offerFromRest:"Flat 20% off on pre-Booking",
+    },
+    {
+        
+        id:2,
+        image:{},
+        restName:"Bismilla",
+        restStarRating:4,
+        restTime:"11pm",
+        restType:"Multi-Cusiene",
+        restLocalAddress:"Annasalai",
+        restCity:"pondicherry",
+        offerFromRest:"",
+    } ,
+    {
+        
+        id:3,
+        image:{},
+        restName:"Banana leaf",
+        restStarRating:4.5,
+        restTime:"11pm",
+        restType:"chinese",
+        restLocalAddress:"Aurovile",
+        restCity:"pondicherry",
+        offerFromRest:"",
+    } ,
+    {
+        
+        id:4,
+        image:{},
+        restName:"velo-cafe",
+        restStarRating:3,
+        restTime:"11pm",
+        restType:"french",
+        restLocalAddress:"busy street",
+        restCity:"pondicherry",
+        offerFromRest:"Flat 20% off on pre-Booking",
+    },
+    {
+       
+        id:5,
+        image:{},
+        restName:"Rocks and rolls",
+        restStarRating:3,
+        restTime:"11pm",
+        restType:"Multi-Cusienes",
+        restLocalAddress:"Ig-Square",
+        restCity:"pondicherry",
+        offerFromRest:"Flat 20% off on pre-Booking",
+    }
+]
